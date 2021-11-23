1ae4f94a3a99607161299d0752b46cb135abbd81
suryaajis330 <suryaajis330@gmail.com>
2021-11-24 03:45:39 +0700

    connection

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 4000;
+
+const { connect } = require("./config/mongodb");
+const MainController = require('./controllers/MainController')
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/sum", MainController)
+app.get("/count", MainController)
+
+
+
+
+
+
+
+
+
+
+
+connect().then(() => {
+  app.listen(port, (_) => {
+    console.log("This mongo server running on port:", port);
+  });
+});
---
+++ config/mongodb.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+
+// connection URL
+const url = process.env.PORT || "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+// Database
+const dbName = "market-db";
+let database;
+
+async function connect() {
+  await client.connect();
+
+  const db = client.db(dbName);
+  database = db;
+
+  console.log("Connect succesfully to server");
+}
+
+function getDatabase() {
+  return database;
+}
+
+module.exports = { connect, getDatabase };
